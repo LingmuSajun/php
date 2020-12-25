1d595fd2c4acf6292a4c845e62edb35decc8252b
鈴木颯駿 <soshunsuzuki@soshun-mb.local>
2020-12-25 23:38:27 +0900

    cssあてる

--- edit.php
+++ edit.php
@@ -1,15 +1,29 @@
 <!doctype html>
 <html lang="ja">
-	<head>
-		<!-- Required meta tags -->
-		<meta charset="utf-8">
-		<title>フォームからデータを受け取る</title>
-	</head>
-	<body>
-		<h1>生年月日</h1>
-		<form action="index.php" method="get">
-			<input type="text" name="comment/"><br/>
-			<input type="submit" value="送信">
-		</form>
-	</body>
+<head>
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="css/style.css">
+
+<title>フォームからデータを受け取る</title>
+</head>
+<body>
+<header>
+<h1 class="font-weight-normal">PHP</h1>
+</header>
+
+<main>
+<h2>今日の運勢</h2>
+<pre>
+	<h6>生年月日</h6>
+	<form action="index.php" method="get">
+		<input type="text" name="comment/"><br/>
+		<input type="submit" value="送信">
+	</form>
+</pre>
+</main>
+</body>
 </html>
