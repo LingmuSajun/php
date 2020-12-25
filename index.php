1d43a6a2baf124c22e9e49df6615010398da64ef
鈴木颯駿 <soshunsuzuki@soshun-mb.local>
2020-12-25 23:16:41 +0900

    運勢

--- index.php
+++ index.php
@@ -1,25 +1,27 @@
 <!doctype html>
 <html lang="ja">
-<head>
-<!-- Required meta tags -->
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<head>
+		<!-- Required meta tags -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-<!-- Bootstrap CSS -->
-<link rel="stylesheet" href="css/style.css">
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="css/style.css">
 
-<title>PHP</title>
-</head>
-<body>
-<header>
-<h1 class="font-weight-normal">PHP</h1>
-</header>
-
-<main>
-<h2>Practice</h2>
-<pre>
-<!-- ここにプログラムを記述します -->
-</pre>
-</main>
-</body>
+		<title>フォームからデータを受け取る</title>
+	</head>
+	<body>
+	<?php $birthDate = $_GET["comment/"]; ?>
+	<?php $rand = mt_rand(1, 10); ?>
+	誕生日は<?php print($birthDate); ?>です<br/>
+	<?php
+		$result = '最低';
+		if($birthDate % $rand == 0) {
+			$result = '最高';
+		} elseif($birthDate % $rand == 1) {
+			$result = '普通';
+		}
+	?>
+	今日の運勢は<?php print($result); ?>です
+	</body>
 </html>
